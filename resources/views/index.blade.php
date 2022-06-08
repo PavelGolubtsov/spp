d9196011613f2435d005f3fa33a2d6149c8355f4
PavelGolubtsov <p.golubcov@mail.ru>
2022-06-09 02:24:40 +0300

    Layout v.1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.layout')
+@section('content')
+<div id="content-wrapper" class="d-flex flex-column" style="padding-top: 20px;">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Система управления задачами</h1>
+            </div>
+            <!-- Content Row -->
+            <div class="row">
+                <div class="card shadow mb-4" style="width: 100%;">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Описание проекта</h6>
+                    </div>
+                    <div class="card-body">
+                        <p>Поле для описания проекта.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Content Row -->
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- End of Main Content -->
+@endsection
